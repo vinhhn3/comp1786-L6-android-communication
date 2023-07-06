d883fd5adf79f7dba800a599fbeab39f454d3c51
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-07-06 09:35:09 +0700

    Init project

---
+++ app/src/main/java/com/example/comp1786_l6_android_communication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp1786_l6_android_communication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
